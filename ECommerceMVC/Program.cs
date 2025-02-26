97754f22763861f8e025b80dc532512d7632e82b
Toàn Nguyễn Quốc <3121560093@sv.sgu.edu.vn>
2025-02-26 23:21:08 +0700

    create-project

---
+++ ECommerceMVC/Program.cs
@@ -0,0 +1,63 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.Helpers;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Hshop2023Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HShop"));
+});
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    options.LoginPath = "/KhachHang/DangNhap";
+    options.AccessDeniedPath = "/AccessDenied"; // Đăng nhập rồi nhưng chưa có quyền sẽ chuyển tới địa chỉ này
+
+
+});
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
